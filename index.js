cacbdc62fab0cd5ad14ff1bd8bed68e9dda989a9
guan <guan840912@gmail.com>
2019-10-13 13:17:59 +0800

    update index.js v2.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! for test v2.0 ya~ ya~ ya ~');
 });
 var server = app.listen(3000, function() {
   var host = server.address().address;
